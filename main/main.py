4d81bf9c43cdb5cafc334ff5da3e39c06b062fb0
Joel Lutman <joellutman@gmail.com>
2017-09-24 22:31:02 +0100

    add default vals

--- main/main.py
+++ main/main.py
@@ -1,31 +1,35 @@
 from main.utils.logger import Logger
+from main.utils.main_parser import MainParser
 from main.FileSystemManager import FileSystemManager
 from main.cnn_model import train, predict
 from main.utils.sys_utils import graceful_exit
 
+if __name__ == '__main__':
+    Logger(log_level='INFO')
 
-Logger(log_level='INFO')
+    main_parser = MainParser().register_args().parse_args()
 
-image_directory = 'images'
-model_directory = 'tmp'
-clean_run = 'y'
+    main_parser.img_dir
 
+    image_directory = 'images'
+    model_directory = 'tmp'
+    clean_run = 'y'
 
-if clean_run.upper() == 'Y':
+    if clean_run.upper() == 'Y':
 
-    file_manager = FileSystemManager(image_directory, model_directory)
-    file_manager.clean_run()
+        file_manager = FileSystemManager(image_directory, model_directory)
+        file_manager.clean_run()
 
-    file_manager.data_science_fs(category0='benign', category1='malignant')
-    # file_manager.organise_files(extract_dir, category_rules={'benign': 'SOB_B_.*.png', 'malignant': 'SOB_M_.*.png'})
+        file_manager.data_science_fs(category0='benign', category1='malignant')
+        # file_manager.organise_files(extract_dir, category_rules={'benign': 'SOB_B_.*.png', 'malignant': 'SOB_M_.*.png'})
 
-elif clean_run.upper() == 'N':
-    pass
+    elif clean_run.upper() == 'N':
+        pass
 
-else:
-    graceful_exit()
+    else:
+        graceful_exit()
 
-train(image_directory, model_directory)
+    train(image_directory, model_directory)
 
-prediction, ground_truth = predict(image_directory, model_directory)
+    prediction, ground_truth = predict(image_directory, model_directory)
 
--- main/utils/main_parser.py
+++ main/utils/main_parser.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-
+import os
 
 class MainParser(ArgumentParser):
 
@@ -9,5 +9,6 @@ class MainParser(ArgumentParser):
     def register_args(self):
         self.add_argument("-d", "--img_dir", help="Directory of images")
         self.add_argument("-c", "--clean", help="Retrain model from scratch", action='store_true')
-        self.add_argument("-m", "--model_dir", help="Directory to store model and logs")
-        self.add_argument("-i", "--iterations", type=int, help="Number of training iterations")
+        self.add_argument("-m", "--model_dir", default=os.path.join('tmp'), help="Directory to store model and logs")
+        self.add_argument("-i", "--iterations", type=int, default=2000, help="Number of training iterations")
+        return self
