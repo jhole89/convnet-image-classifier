7db2d9744b0df1a3c013feaddf585de38f6b5f43
Joel Lutman <joellutman@gmail.com>
2017-09-25 12:21:32 +0100

    make img_dir required

--- main/utils/main_parser.py
+++ main/utils/main_parser.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-import os
+
 
 class MainParser(ArgumentParser):
 
@@ -7,8 +7,9 @@ class MainParser(ArgumentParser):
         super().__init__()
 
     def register_args(self):
-        self.add_argument("-d", "--img_dir", help="Directory of images")
+        self.add_argument("img_dir", help="Directory of images")
         self.add_argument("-c", "--clean", help="Retrain model from scratch", action='store_true')
-        self.add_argument("-m", "--model_dir", default=os.path.join('tmp'), help="Directory to store model and logs")
+        self.add_argument("-m", "--model_dir", default='tmp', help="Directory to store model and logs")
         self.add_argument("-i", "--iterations", type=int, default=2000, help="Number of training iterations")
         return self
+
--- test/utils/test_main_parser.py
+++ test/utils/test_main_parser.py
@@ -6,13 +6,20 @@ def test_main_parser():
     parser = MainParser()
     parser.register_args()
 
-    args_img = ['-d', 'test_img_dir']
-    args_model = ['-m', 'test_model_dir']
-    args_iterations = ['-i', '2000']
-    args_options = ['-c']
-    opts = parser.parse_args(args_img + args_model + args_iterations + args_options)
+    default_args = parser.parse_args(['/my/test/directory'])
 
-    assert opts.img_dir == args_img[1]
-    assert opts.model_dir == args_model[1]
-    assert opts.iterations == int(args_iterations[1])
-    assert opts.clean is True
+    assert default_args.img_dir == '/my/test/directory'
+    assert default_args.model_dir == 'tmp'
+    assert default_args.iterations == 2000
+    assert default_args.clean is False
+
+    args_img = 'test_img_dir'
+    args_model = ['-m', 'test_model_dir']
+    args_iterations = ['-i', '500']
+    args_options = ['-c']
+    parsed_args = parser.parse_args([args_img] + args_model + args_iterations + args_options)
+
+    assert parsed_args.img_dir == args_img
+    assert parsed_args.model_dir == args_model[1]
+    assert parsed_args.iterations == int(args_iterations[1])
+    assert parsed_args.clean is True
