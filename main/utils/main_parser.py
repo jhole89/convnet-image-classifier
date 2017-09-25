bed7a41fc1eee8711104e063adbd561d375609cd
Joel Lutman <joellutman@gmail.com>
2017-09-25 15:24:14 +0100

    add checking for positive iterations only

--- main/utils/main_parser.py
+++ main/utils/main_parser.py
@@ -11,7 +11,8 @@ class MainParser(ArgumentParser):
         self.add_argument("img_dir", type=self.valid_path, help="Directory of images")
         self.add_argument("-c", "--clean", help="Retrain model from scratch", action='store_true')
         self.add_argument("-m", "--model_dir", default='tmp', help="Directory to store model and logs")
-        self.add_argument("-i", "--iterations", type=int, default=2000, help="Number of training iterations")
+        self.add_argument("-i", "--iterations", type=self.positive_int,
+                          default=2000, help="Number of training iterations")
         return self
 
     @staticmethod
@@ -19,3 +20,10 @@ class MainParser(ArgumentParser):
         if not os.path.exists(path):
             raise OSError
         return path
+
+    @staticmethod
+    def positive_int(value):
+        integer_val = int(value)
+        if integer_val <= 0:
+            raise ValueError
+        return integer_val
