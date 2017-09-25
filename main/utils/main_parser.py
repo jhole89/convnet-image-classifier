93fa76b80fe18f253890b270a5a502d832f33629
Joel Lutman <joellutman@gmail.com>
2017-09-25 13:32:26 +0100

    add path checking for user defined paths

--- main/utils/main_parser.py
+++ main/utils/main_parser.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+import os
 
 
 class MainParser(ArgumentParser):
@@ -7,9 +8,14 @@ class MainParser(ArgumentParser):
         super().__init__()
 
     def register_args(self):
-        self.add_argument("img_dir", help="Directory of images")
+        self.add_argument("img_dir", type=self.valid_path, help="Directory of images")
         self.add_argument("-c", "--clean", help="Retrain model from scratch", action='store_true')
         self.add_argument("-m", "--model_dir", default='tmp', help="Directory to store model and logs")
         self.add_argument("-i", "--iterations", type=int, default=2000, help="Number of training iterations")
         return self
 
+    @staticmethod
+    def valid_path(path):
+        if not os.path.exists(path):
+            raise OSError
+        return path
--- test/utils/test_main_parser.py
+++ test/utils/test_main_parser.py
@@ -1,25 +1,26 @@
 from main.utils.main_parser import MainParser
 
 
-def test_main_parser():
+def test_main_parser(tmpdir):
 
     parser = MainParser()
     parser.register_args()
 
-    default_args = parser.parse_args(['/my/test/directory'])
+    args_img = str(tmpdir)
+    args_model = ['-m', 'test_model_dir']
+    args_iterations = ['-i', '500']
+    args_options = ['-c']
 
-    assert default_args.img_dir == '/my/test/directory'
+    default_args = parser.parse_args([args_img])
+
+    assert default_args.img_dir == tmpdir
     assert default_args.model_dir == 'tmp'
     assert default_args.iterations == 2000
     assert default_args.clean is False
 
-    args_img = 'test_img_dir'
-    args_model = ['-m', 'test_model_dir']
-    args_iterations = ['-i', '500']
-    args_options = ['-c']
     parsed_args = parser.parse_args([args_img] + args_model + args_iterations + args_options)
 
-    assert parsed_args.img_dir == args_img
+    assert parsed_args.img_dir == tmpdir
     assert parsed_args.model_dir == args_model[1]
     assert parsed_args.iterations == int(args_iterations[1])
     assert parsed_args.clean is True
