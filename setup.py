6e27b3f4f6d053a783bdc718bfaad5f40d75b6b4
Joel Lutman <joellutman@gmail.com>
2017-11-22 20:58:01 +0000

    changed version for setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def get_requirements(fname):
+    return read(fname).splitlines()
+
 
 setup(
-    name='convet-image-classifier',
-    version='1.0.0',
+    name='img-classifier',
+    version='0.1.0',
     packages=['main', 'main.utils'],
-    url='https://github.com/jhole89/convet-image-classifier',
+    url='https://github.com/jhole89/convnet-image-classifier',
     license='GPL',
     author='Joel Lutman',
     author_email='joellutman@gmail.com',
-    description='A Convolutional Neural Network utility for image recognition'
+    description='A Convolutional Neural Network utility for image recognition',
+    long_description=read('README.md'),
+    install_requires=get_requirements('requirements.txt')
 )
